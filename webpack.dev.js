03909e9f98c9a7334e799c21d48e6d97e15e13bf
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-12-31 09:49:13 -0800

    add webpack.dev.js for dev configs

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const webpackConfig = require("./webpack.config");
+
+module.exports = merge(webpackConfig, {
+    mode: "development",
+    entry: "./src/index.tsx",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        compress: true,
+        port: 3000,
+        open: true,
+        hot: true
+    }
+});
