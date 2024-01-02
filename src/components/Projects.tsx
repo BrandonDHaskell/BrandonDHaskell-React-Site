5798ca6ad38abd062b3ca674e7a9bed8e9043e19
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-01 22:00:23 -0800

    add Proejcts file and initial code

---
+++ src/components/Projects.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+interface ProjectProps {
+
+}
+
+const Projects: React.FC<ProjectProps> = () => {
+    return (
+        <section id="projects">
+            Project go here.
+        </section>
+
+    )
+}
