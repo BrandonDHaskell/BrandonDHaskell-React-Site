30dd7cd046956cdab213202c987dd79de3577b09
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-01 21:27:58 -0800

    refactor index.ts for React 18 createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App message="BrandonDHaskell" />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("No root element found!");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <App 
+            message=""
+        />
+    </React.StrictMode>
+);
