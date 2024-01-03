598cdd58fea710f59c484e09c7a2ea154d5f53b0
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-03 09:50:23 -0800

    add basic html form

--- src/components/EmailMe.tsx
+++ src/components/EmailMe.tsx
@@ -23,8 +23,44 @@ const EmailMe: React.FC = () => {
     };
 
     return (
-        <section>
-            Email me section.
+        <section id="connect-with-me" className="column justify-center">
+            <h2 className="mb-8 mt-8 text 4xl font-bold md:text-5xl lg:text-6xl">Email Me</h2>
+            {emailSent ? (
+                <div>
+                    <p>Email sent!</p>
+                    <p>Feel free to connect with my on other social platforms:</p>
+                    <p><a href="https://linkedin.com/in/BrandonDHaskell">in/BrandonDHaskell</a></p>
+                    <p><a href="https://x.com/BrandonDHaskell">@BrandonDHaskell</a></p>
+                </div>
+            ) : (
+                <form onSubmit={sendEmail}>
+                    <div className="flex justify-center outline-sky-500">
+                        <div className="text-sky-500 px-2">
+                            <label htmlFor="name">Name</label>
+                        </div>
+                        <div>
+                            <input type="text" id="name" name="name" required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                        </div>
+                    </div>
+                    <div className="flex justify-center outline-sky-500">
+                        <div className="text-sky-500 px-2">
+                            <label htmlFor="email">Email</label>
+                        </div>
+                        <div className="text-sky-500 px-2"> 
+                            <input type="email" id="email" name="email" required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                        </div>
+                    </div>
+                    <div className="flex justify-center outline-sky-500">
+                        <div className="text-sky-500 px-2">
+                            <label htmlFor="message">Message</label>
+                        </div>
+                        <div className="text-sky-500 px-2">
+                            <textarea id="message" name="message" rows={5} required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                        </div>
+                    </div>
+                    <button type="submit" className="m-2 px-4 py-2 font-semibold text-sm bg-white text-slate-700 border border-slate-300 rounded-md shadow-sm outline outline-2 outline-offset-0 outline-sky-500 dark:bg-slate-700 dark:text-slate-200 dark:border-transparent">Send</button>
+                </form>
+            )}
         </section>
     );
 }
