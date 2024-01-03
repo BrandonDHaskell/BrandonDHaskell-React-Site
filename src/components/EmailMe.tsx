c58e0b63de451059fcd9b3b78622221fec8e0523
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-03 10:17:19 -0800

    add sending messaging feature during email send

--- src/components/EmailMe.tsx
+++ src/components/EmailMe.tsx
@@ -3,9 +3,11 @@ import emailjs from "@emailjs/browser";
 
 const EmailMe: React.FC = () => {
     const [emailSent, setEmailSent] = useState(false);
+    const [isSending, setIsSending] = useState(false);
 
     const sendEmail = (emailFormEvent: React.FormEvent<HTMLFormElement>) => {
         emailFormEvent.preventDefault();
+        setIsSending(true);
         emailjs.sendForm(
             'service_o31wbxh',
             'template_profile_site',
@@ -14,6 +16,7 @@ const EmailMe: React.FC = () => {
         )
         .then(
             () => {
+                setIsSending(false);
                 setEmailSent(true);
             },
             (error: Error) => {
@@ -33,33 +36,39 @@ const EmailMe: React.FC = () => {
                     <p><a href="https://x.com/BrandonDHaskell">@BrandonDHaskell</a></p>
                 </div>
             ) : (
-                <form onSubmit={sendEmail}>
-                    <div className="flex justify-center outline-sky-500">
-                        <div className="text-sky-500 px-2">
-                            <label htmlFor="name">Name</label>
+                <>
+                    {isSending ? (
+                        <div className="sending-email">Sending email...</div>
+                    ) : (
+                    <form onSubmit={sendEmail}>
+                        <div className="flex justify-center outline-sky-500">
+                            <div className="text-sky-500 px-2">
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div>
+                                <input type="text" id="name" name="name" required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                            </div>
                         </div>
-                        <div>
-                            <input type="text" id="name" name="name" required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                        <div className="flex justify-center outline-sky-500">
+                            <div className="text-sky-500 px-2">
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="text-sky-500 px-2"> 
+                                <input type="email" id="email" name="email" required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex justify-center outline-sky-500">
-                        <div className="text-sky-500 px-2">
-                            <label htmlFor="email">Email</label>
+                        <div className="flex justify-center outline-sky-500">
+                            <div className="text-sky-500 px-2">
+                                <label htmlFor="message">Message</label>
+                            </div>
+                            <div className="text-sky-500 px-2">
+                                <textarea id="message" name="message" rows={5} required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
+                            </div>
                         </div>
-                        <div className="text-sky-500 px-2"> 
-                            <input type="email" id="email" name="email" required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
-                        </div>
-                    </div>
-                    <div className="flex justify-center outline-sky-500">
-                        <div className="text-sky-500 px-2">
-                            <label htmlFor="message">Message</label>
-                        </div>
-                        <div className="text-sky-500 px-2">
-                            <textarea id="message" name="message" rows={5} required className="border-2 rounded border-slate-300 text-sky-500 px-2 m-2" />
-                        </div>
-                    </div>
-                    <button type="submit" className="m-2 px-4 py-2 font-semibold text-sm bg-white text-slate-700 border border-slate-300 rounded-md shadow-sm outline outline-2 outline-offset-0 outline-sky-500 dark:bg-slate-700 dark:text-slate-200 dark:border-transparent">Send</button>
-                </form>
+                        <button type="submit" className="m-2 px-4 py-2 font-semibold text-sm bg-white text-slate-700 border border-slate-300 rounded-md shadow-sm outline outline-2 outline-offset-0 outline-sky-500 dark:bg-slate-700 dark:text-slate-200 dark:border-transparent">Send</button>
+                    </form>
+                    )}
+                </>
             )}
         </section>
     );
