c4b8741208200ab63acafb08a9cd9194da19e448
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-03 09:31:40 -0800

    add EmailJS and state to EmailMe component

--- src/components/EmailMe.tsx
+++ src/components/EmailMe.tsx
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const EmailMe: React.FC = () => {
+    const [emailSent, setEmailSent] = useState(false);
+
+    const sendEmail = (emailFormEvent: React.FormEvent<HTMLFormElement>) => {
+        emailFormEvent.preventDefault();
+        emailjs.sendForm(
+            'service_o31wbxh',
+            'template_profile_site',
+            emailFormEvent.currentTarget,
+            '3WOn7ZaKmoc_hnHYt'
+        )
+        .then(
+            () => {
+                setEmailSent(true);
+            },
+            (error: Error) => {
+                console.log("Email send failure:", error.message);
+            }
+        );
+    };
 
     return (
         <section>
