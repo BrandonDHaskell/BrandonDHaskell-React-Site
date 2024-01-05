e4fad7a335fc377b3b9690b330741e3b8ee732d4
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-05 15:25:36 -0800

    delete old folder structure
    	deleted:    client/public/images/Profile_Pic_2_small.jpg
    	deleted:    client/public/images/day-scheduler.png
    	deleted:    client/public/images/know-your-government.png
    	deleted:    client/public/images/pswd-generator.png
    	deleted:    client/public/images/quiz-time.png
    	deleted:    client/public/images/weather-planner.png

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
@@ -9,13 +10,22 @@ const PORT: number | string = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended : true }));
 
+// Serve my static web page
+app.use(express.static(path.join(__dirname, "../../client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
 const startServer = async () => {
     try {
         app.listen(PORT, () => {
-            console.log(`Server running on port: {PORT}`);
+            console.log(`Server running on port: ${PORT}`);
         })
     } catch (err) {
         console.log("Error starting server");
         console.log(err);
     }
 }
+
+startServer();
