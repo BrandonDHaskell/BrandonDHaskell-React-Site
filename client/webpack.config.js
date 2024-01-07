5458f2d103b9eb9be83759396fa318a1f361297d
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-07 10:21:03 -0800

    update webpack config for webp format images

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/i,
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
                 type: "asset/resource",
                 generator: {
                     filename: "images/[name][ext][query]"
