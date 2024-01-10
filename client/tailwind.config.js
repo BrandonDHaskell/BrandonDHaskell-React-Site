0e2be1cca2e4e57f066dc6221026a07e7d4e263a
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-10 14:57:39 -0800

    update css for animation of text

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        keyframes: {
+            slideIn: {
+                "0%": { transform: "translateX(100%)", opacity: 0 },
+                "100%": { transform: "translateX(0)", opacity: 1 }
+            }
+        },
+        animation: {
+            slideIn: "slideIn 1s ease-out"
+        }
+    },
   },
   plugins: [],
 }
