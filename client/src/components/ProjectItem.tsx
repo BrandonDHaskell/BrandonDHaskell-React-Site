f1548d2dced949c97d7f2cd5f830808f3edc9537
Mike-415 <mgonz160@mail.ccsf.edu>
2024-01-18 14:10:01 -0800

    Anchored the Source Code link to the bottom of the Project Items

--- client/src/components/ProjectItem.tsx
+++ client/src/components/ProjectItem.tsx
@@ -19,23 +19,23 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
 }) => {
     return (
         <div className="project-card rounded p-8 m-6 max-w-xs ring-2 ring-offset-2 ring-stone-900 shadow-2xl shadow-gray-400/75 hover:shadow-sky-500">
-            <a href={liveSiteLink}>
-                <div className="project-info rounded">
+            <a href={liveSiteLink} className="flex flex-col h-full">
+                <div className="project-info rounded flex-1">
                     <h3 className="project-title">{name}</h3>
                     <p className="project-desc text-sm italic pb-5">{summary}</p>
-                    <img className="rounded border-2 pb-4 mb-4" src={imgSrc} alt={name} />
+                    <img className="rounded border-2 mb-4" src={imgSrc} alt={name} />
                     <div className="flex flex-wrap justify-center py-1">
                         {techList.map((tech, index) => (
                             <span key={index} className="text-white bg-sky-700 px-2 py-1 ml-2 mb-1 mt-1 rounded-md block">
-                            {tech}
-                        </span>
+                                {tech}
+                            </span>
                         ))}
                     </div>
                 </div>
+                <div className="flex justify-center text-skyl-400 py-3">
+                    <a href={sourceCodeLink} className="mt-auto">Source code</a>
+                </div>
             </a>
-            <div className="flex justify-center text-skyl-400 py-3">
-                <a href={sourceCodeLink}>Source code</a>
-            </div>
         </div>
     );
 };
