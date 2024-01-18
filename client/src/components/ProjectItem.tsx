85f85fbdb1dceb839c5b6dcfc76d9a9955e39bf5
Mike-415 <mgonz160@mail.ccsf.edu>
2024-01-18 12:49:23 -0800

    Updated the project item's tech stack badges

--- client/src/components/ProjectItem.tsx
+++ client/src/components/ProjectItem.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 interface ProjectItemProps {
-    name: string,
-    summary: string,
-    imgSrc: string,
-    liveSiteLink: string,
-    sourceCodeLink: string,
-    techList: string[]
+    name: string;
+    summary: string;
+    imgSrc: string;
+    liveSiteLink: string;
+    sourceCodeLink: string;
+    techList: string[];
 }
 
 const ProjectItem: React.FC<ProjectItemProps> = ({
@@ -18,15 +18,17 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
     techList
 }) => {
     return (
-        <div className="project-card rounded p-8 m-6 max-w-xs rign-2 ring-offset-2 ring-stone-900 shadow-2xl shadow-gray-400/75 hover:shadow-sky-500">
+        <div className="project-card rounded p-8 m-6 max-w-xs ring-2 ring-offset-2 ring-stone-900 shadow-2xl shadow-gray-400/75 hover:shadow-sky-500">
             <a href={liveSiteLink}>
                 <div className="project-info rounded">
                     <h3 className="project-title">{name}</h3>
-                    <p className="project-desc text-sm italic  pb-5">{summary}</p>
-                    <img className="rounded boarder-2 border-stone-900" src={imgSrc} alt={name} />
+                    <p className="project-desc text-sm italic pb-5">{summary}</p>
+                    <img className="rounded border-2 pb-4 mb-4" src={imgSrc} alt={name} />
                     <div className="flex flex-wrap justify-center py-1">
                         {techList.map((tech, index) => (
-                            <div key={index}><span className="box-decoration-clone bg-gradient-to-r from-yellow-600 to-sky-700 px-2 mx-2 rounded-lg">{tech}</span></div>
+                            <span key={index} className="text-white bg-sky-700 px-2 py-1 ml-2 mb-1 mt-1 rounded-md block">
+                            {tech}
+                        </span>
                         ))}
                     </div>
                 </div>
@@ -35,7 +37,7 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
                 <a href={sourceCodeLink}>Source code</a>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ProjectItem;
